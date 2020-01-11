1bb4fa4f468493d14899394018ce660071c3845c
Johnny Souto <johnny.souto@icloud.com>
2020-01-11 15:32:57 -0300

    update main.js

--- src/www/js/main.js
+++ src/www/js/main.js
@@ -1,1 +1,18 @@
 // main.js
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+function selectItem(e){
+    // Remove border to previus tab
+    removeBorder();
+    // Add border to current tab
+    this.classList.add('tab-border');
+}
+
+function removeBorder(){
+    tabItems.forEach(item => item.classList.remove('tab-border'));
+}
+
+// Listen for tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
